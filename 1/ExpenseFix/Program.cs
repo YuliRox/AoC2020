c0b91953ec3977e2e742c8607c8c45fc07d8e188
Sandra Schuhmacher <Sandra.Schuhmacher@sva.de>
2020-12-21 00:05:38 +0100

    Added Part 2 of AoC 1

--- 1/ExpenseFix/Program.cs
+++ 1/ExpenseFix/Program.cs
@@ -13,6 +13,8 @@ namespace ExpenseFix
             var inputNumbers = inputLines.Select(int.Parse).ToArray();
 
             var cartesianAddTwo = inputNumbers
+                //          |func1: _=> inputNumbers // nimm für jede zahl wieder die komplette liste an zahlen
+                //                            | func2: x aktuelle zahl, y wird für alle zahlen aus der zweiten liste durchiteriert
                 .SelectMany(_ => inputNumbers, (x, y) => new
                 {
                     A = x,
@@ -20,13 +22,39 @@ namespace ExpenseFix
                     Add = x + y,
                     Multiple = x * y
                 });
+
+            var cartesianAddThree = inputNumbers
+                .SelectMany(_ => inputNumbers, (x, y) => new
+                {
+                    A = x,
+                    B = y
+                })
+                .SelectMany(_ => inputNumbers, (x, y) => new
+                {
+                    A = x.A,
+                    B = x.B,
+                    C = y,
+                    Add = x.A + x.B + y,
+                    Multiple = x.A * x.B * y
+                });
+
             var resultSet = cartesianAddTwo
             .Where(x => x.Add == 2020)
             .ToList();
 
+            Console.WriteLine($"--- Part One ---");
             foreach (var result in resultSet)
             {
                 Console.WriteLine($"{result.A}*{result.B}={result.Multiple}");
+            }
+
+            var resultSet2 = cartesianAddThree
+            .Where(x => x.Add == 2020)
+            .ToList();
+            Console.WriteLine($"--- Part Two ---");
+            foreach (var result in resultSet2)
+            {
+                Console.WriteLine($"{result.A}*{result.B}*{result.C}={result.Multiple}");
             }
         }
     }
