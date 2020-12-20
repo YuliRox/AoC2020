f4249859267237c7bd14740fd6e0ba799e01e6ed
Xemrox <xemrox@users.noreply.github.com>
2020-12-20 23:07:28 +0100

    added day 1 part 1

---
+++ 1/ExpenseFix/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseFix
+{
+    public static class Program
+    {
+        public static async Task Main(string[] _)
+        {
+            var inputLines = await File.ReadAllLinesAsync("../input.txt");
+            var inputNumbers = inputLines.Select(int.Parse).ToArray();
+
+            var cartesianAddTwo = inputNumbers
+                .SelectMany(_ => inputNumbers, (x, y) => new
+                {
+                    A = x,
+                    B = y,
+                    Add = x + y,
+                    Multiple = x * y
+                });
+            var resultSet = cartesianAddTwo
+            .Where(x => x.Add == 2020)
+            .ToList();
+
+            foreach (var result in resultSet)
+            {
+                Console.WriteLine($"{result.A}*{result.B}={result.Multiple}");
+            }
+        }
+    }
+}
